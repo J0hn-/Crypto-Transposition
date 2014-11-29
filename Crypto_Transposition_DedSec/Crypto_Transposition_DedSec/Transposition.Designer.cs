a2cd42cff8109996ef9fc76d2fadd5fd2151f3ed
Jonathan Platteau <jonathan.platteau@yahoo.fr>
2014-11-29 01:12:13 +0100

    Premiere interface !

---
+++ Crypto_Transposition_DedSec/Crypto_Transposition_DedSec/Transposition.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Crypto_Transposition_DedSec
+{
+    partial class Transposition
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Transposition));
+            this.SuspendLayout();
+            // 
+            // Transposition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(783, 521);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(799, 559);
+            this.Name = "Transposition";
+            this.Text = "CRYP70 7R4NSP051710N By D3D55C - J0HN & B4TR0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Crypto_Transposition_DedSec/Crypto_Transposition_DedSec/Transposition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crypto_Transposition_DedSec
+{
+    public partial class Transposition : Form
+    {
+        public Transposition()
+        {
+            InitializeComponent();
+        }
+    }
+}
